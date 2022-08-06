6677c1d952eddca201c60a7133f3a91d04c3d215
unknown <syunyasisotto@gmail.com>
2022-08-06 18:35:31 +0900

    エンドポイント作成した

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # from unicodedata import name
 # from fashionpedia.fp import FashionPedia
-# import numpy as np
-# import os
-from flask import Flask
+import numpy as np
+import os
+from flask import Flask, request ,send_file
+import cv2
+import io
+from google.cloud import storage
+from google.cloud import Blob
+
 
 app = Flask(__name__)
 # img = "../assets/test1.jpg"
@@ -13,7 +18,31 @@ def test():
     print('aaaa')
     return "Hello"
 
-
+@app.route('/get_image',methods = ['POST'])
+def return_image():
+    # 画像をとってくる
+    if request.files['image']:
+        filename = request.files['image'].filename
+        content_type = request.files['image'].content_type
+        # 画像として読み込み
+        stream = request.files['image'].stream
+        img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
+        img = cv2.imdecode(img_array, 1)
+        issuccess,buffer = cv2.imencode('.jpg',img)
+        buf = io.BytesIO(buffer)
+        buf.seek(0)
+        
+        # こっから下はfirestorage関連でしゅ
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './fashionpedia-.json'
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket('fashionpedia')
+        blob = Blob(filename, bucket)
+        blob.upload_from_file(data=buf.getvalue(),content_type=content_type)
+        
+        return send_file(
+            buf,attachment_filename=filename,as_attachment=True
+        )
+        
 if __name__ == '__main__':
     app.run()
 
