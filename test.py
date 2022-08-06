3091338203fbc38ae1aacbcae6afd60acef854b3
unknown <syunyasisotto@gmail.com>
2022-08-06 15:51:29 +0900

    flask test

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
+from unicodedata import name
 from fashionpedia.fp import FashionPedia
 import numpy as np
 import os
+from flask import Flask
+
+app = Flask(__name__)
 img = "../assets/test1.jpg"
+
+@app.route('/')
+def test():
+    return "Hello"
+
+if __name__ == '__main__':
+    app.run()
 
